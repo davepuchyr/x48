62b5bfcd567080870a08e462b663e5623923146d
Dave Puchyr <dave@debt.avaritia.com>
2019-03-12 20:03:38 +0100

    Added a top-level directory to the tar file.

--- fedora.sh
+++ fedora.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+NAME_VERSION="x48-0.6.4"
+
 ./autogen.sh
 ./configure
-tar -cvzf x48-0.6.4.tar.gz aclocal.m4 AUTHORS ChangeLog compile config* COPYING debian depcomp INSTALL install-sh Makefile* missing NEWS README rpm src version.m4
-cd rpm && rpmbuild -bb x48.spec
+rm -rf $NAME_VERSION && mkdir $NAME_VERSION
+cp -ar aclocal.m4 AUTHORS ChangeLog compile config* COPYING debian depcomp INSTALL install-sh Makefile* missing NEWS README romdump rpm src version.m4 $NAME_VERSION
+tar -cvzf ~/rpmbuild/SOURCES/$NAME_VERSION.tar.gz $NAME_VERSION
+( cd rpm && rpmbuild -bb x48.spec )
+rm -rf $NAME_VERSION
 
