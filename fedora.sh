c8ea1605051594c143a552080402a092eb6aef41
Dave Puchyr <dave@debt.avaritia.com>
2019-03-13 12:28:06 +0100

    Added x48.png.

--- fedora.sh
+++ fedora.sh
@@ -6,7 +6,7 @@ NAME_VERSION="x48-0.6.4"
 ./autogen.sh
 ./configure
 rm -rf $NAME_VERSION && mkdir $NAME_VERSION
-cp -ar aclocal.m4 AUTHORS ChangeLog compile config* COPYING debian depcomp INSTALL install-sh Makefile* missing NEWS README romdump rpm src version.m4 $NAME_VERSION
+cp -ar aclocal.m4 AUTHORS ChangeLog compile config* COPYING debian depcomp INSTALL install-sh Makefile* missing NEWS README romdump rpm src version.m4 x48.png $NAME_VERSION
 tar -cvzf ~/rpmbuild/SOURCES/$NAME_VERSION.tar.gz $NAME_VERSION
 ( cd rpm && rpmbuild --verbose -bb x48.spec )
 rm -rf $NAME_VERSION
