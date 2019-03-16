835512169390f28262b4deac84765fdc0ff83336
Dave Puchyr <dave@debt.avaritia.com>
2019-03-16 14:59:13 +0100

    Moved romdumps to /usr/share/x48; wrapped the x48 binary with a script that manages initialization.

--- fedora.sh
+++ fedora.sh
@@ -6,11 +6,9 @@ NAME_VERSION="x48-0.6.4"
 ./autogen.sh
 ./configure
 rm -rf $NAME_VERSION && mkdir $NAME_VERSION
-cp -ar aclocal.m4 AUTHORS ChangeLog compile config* COPYING debian depcomp INSTALL install-sh Makefile* missing NEWS README romdump rpm src version.m4 x48.png $NAME_VERSION
+cp -ar aclocal.m4 AUTHORS ChangeLog compile config* COPYING depcomp INSTALL install-sh Makefile* missing NEWS README ROMs/* romdump rpm src version.m4 x48.png $NAME_VERSION
 tar -cvzf ~/rpmbuild/SOURCES/$NAME_VERSION.tar.gz $NAME_VERSION
 ( cd rpm && rpmbuild --verbose -bb x48.spec )
 rm -rf $NAME_VERSION
 sudo dnf -y install ~/rpmbuild/RPMS/x86_64/$NAME_VERSION*.rpm
-mkdir -p ~/.hp48
-cp -av ROMs/* ~/.hp48
-( cd ~/.hp48 && ln -svf gxrom-r rom && ln -svf rom rom.dump && if [ ! -s hp48 ] ; then x48 -initialize ; fi )
+x48
